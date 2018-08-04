30e2f843d95b2cbbb9103e46405f47d427c88bae
Unknown <uk_space@hotmail.com>
2018-08-04 14:23:04 +0100

    Added database and second page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from home.models import Contract
+from django.http import HttpResponseRedirect
 
 
 def index(request):
+    if request.method == 'POST':
+        form_first_name = request.POST['name']
+        form_initials = request.POST['initials']
+
+        Contract.objects.create(fname=form_first_name, initial=form_initials)
+        print(Contract.objects.all())
+
+        return render(request, 'home/game.html')
+
     return render(request, 'home/home.html',)
